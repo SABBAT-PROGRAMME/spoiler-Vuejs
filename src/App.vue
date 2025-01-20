f6ea143d15829bba1964679f1ba08b116c4ccf6b
= <=>
2025-01-20 13:32:19 +0100

    add component TransitionGroup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
-<!-- tamplate -->
 <template>
   <h1>Hello World !</h1>
 
-  <button @click="toggleSpoiler">
+  <button @click="toggleSpoiler" class="spoiler">
     {{ showSpoiler ? "Cacher le spoiler" : "Montrer le spoiler" }}
   </button>
 
-  <Transition>
+  <FadeSlideTrasition>
     <div v-if="showSpoiler" class="spoiler">
-      A la fin de la serie l'acteur principal meurt !
+      À la fin de la série, l'acteur principal meurt !
     </div>
-  </Transition>
+  </FadeSlideTrasition>
+
+  <hr />
+
+  <form action="" role="group" @submit.prevent="addMovie">
+    <input type="text" placeholder="Ajouter un film" v-model="movie" />
+    <button :disabled="movie.length === 0">Ajouter</button>
+  </form>
+
+  <ul>
+    <TransitionGroup name="list">
+      <li v-for="(movie, index) in movies" :key="index">
+        {{ movie }}
+        <button @click="removeMovie(movie)" class="delete">X</button>
+      </li>
+    </TransitionGroup>
+  </ul>
 </template>
 
-<!-- script -->
 <script setup>
 import { ref } from "vue";
+import FadeSlideTrasition from "./FadeSlideTrasition.vue";
 
 const showSpoiler = ref(false);
 const toggleSpoiler = () => (showSpoiler.value = !showSpoiler.value);
+
+const movie = ref("");
+const movies = ref(["Titanic", "Un prince à New-York", "Avatar", "Matrix"]);
+
+// methods
+
+const addMovie = () => {
+  movies.value.push(movie.value);
+  movie.value = "";
+};
+
+const removeMovie = (movie) => {
+  movies.value = movies.value.filter((m) => m !== movie);
+};
 </script>
 
-<!-- style -->
 <style>
 .spoiler {
   padding: 1rem;
@@ -30,8 +58,23 @@ const toggleSpoiler = () => (showSpoiler.value = !showSpoiler.value);
   transition: 0.5s;
 }
 
-v-enter-from,
-v-leave-to {
+.delete {
+  background-color: gray;
+  border: none;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+/* .list-leave-active {
+  position: absolute;
+} */
+
+.list-enter-from,
+.list-leave-to {
   opacity: 0;
+  transform: translateX(30px);
 }
 </style>
---
+++ src/FadeSlideTrasition.vue
@@ -0,0 +1,18 @@
+<!-- tamplate -->
+<template>
+  <Transition name="fade">
+    <slot></slot>
+  </Transition>
+</template>
+
+<!-- style -->
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+fade-enter-from,
+fade-leave-to {
+  opacity: 0;
+}
+</style>
